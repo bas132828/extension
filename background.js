69ae179f4997069c1f5b7f87f3f4b81c35082545
Andrei <1328bas@gmail.com>
2021-07-21 11:54:35 +0300

    interaction with booking website

--- background.js
+++ background.js
@@ -3,8 +3,19 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log(`color changed on ${color}`);
 });
-
-// chrome.runtime.onInstalled.addListener(() => {
-//     chrome.storage.sync.set({ color });
-//     console.log('Default background color set to %cgreen', `color: ${color}`);
-//   });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  //   console.log(tabId, changeInfo, tab);
+  if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+    chrome.scripting
+      .executeScript({
+        target: {
+          tabId,
+        },
+        files: ["./foreground.js"],
+      })
+      .then(() => {
+        console.log("injected the foreground script");
+      })
+      .catch((e) => console.log(e));
+  }
+});
--- foreground.js
+++ foreground.js
@@ -1,1 +1,8 @@
 console.log("from front");
+
+const logo = document.querySelector(
+  "#b2indexPage > header > nav.bui-header__bar > div.bui-group.bui-button-group.bui-group--inline.bui-group--align-end.bui-group--vertical-align-middle > div:nth-child(2) > button > span > div > img"
+);
+console.log(logo);
+logo.style.width = 0;
+logo.style.transition = "4s";

b52c5c3a1ff22b7ac475e6e8f4d0ad2bd6868aa6
Andrei <1328bas@gmail.com>
2021-07-25 12:06:34 +0300

    changed css styles

--- foreground.js
+++ foreground.js
@@ -1,9 +1,28 @@
-const names = Array.from(document.querySelectorAll(".sr-hotel__title"));
-const firstFive = names.slice(0, 5);
-
-const temp = firstFive.map((el) => {
-  return el.innerText.split(" ").slice(0, -4).join(" ");
+const nodesOnViewPort = Array.from(
+  document.querySelectorAll(
+    "#hotellist_inner > div.sr_item.sr_item_new.sr_item_default.sr_property_block"
+  )
+);
+const firstFiveNodes = nodesOnViewPort.slice(0, 5);
+console.log(firstFiveNodes);
+const arrayToSend = firstFiveNodes.map((el, i) => {
+  // console.log(el.querySelector(".sr-hotel__name").textContent);
+  // console.log(el.querySelector("img").getAttribute("src"));
+  // console.log(el.querySelector("div.bui-price-display__value").textContent);
+  // console.log(el.querySelector("a").getAttribute("href"));
+  return {
+    title: el.querySelector(".sr-hotel__name").textContent,
+    img: el.querySelector("img").getAttribute("src"),
+    price: el.querySelector("div.bui-price-display__value").textContent,
+    link: `https://booking.com/${el.querySelector("a").getAttribute("href")}`,
+    id: i,
+  };
 });
-const firstFiveText = temp.reverse();
+console.log(arrayToSend);
+// console.log(arrayToSend);
+// const temp = firstFive.map((el) => {
+//   return;
+// });
+// const firstFiveText = temp.reverse();
 
-chrome.storage.sync.set({ firstFiveText });
+chrome.storage.sync.set({ arrayToSend });
--- popup.js
+++ popup.js
@@ -1,9 +1,21 @@
-chrome.storage.sync.get(["firstFiveText"], function (result) {
-  result.firstFiveText.forEach((el, i) => {
+// chrome.storage.sync.get(["arrayToSend"], function (result) {
+//   console.log(result);
+// });
+
+chrome.storage.sync.get(["arrayToSend"], function (result) {
+  result.arrayToSend.forEach((el) => {
     const html = `
-      <ul class='ce_container--name'>
-          <li> ${el} </li>
-          <button class='change-text--button' id='id-button-${i}'>Get this hotel</button >
+      <ul class='ce_container'>
+        <a class='cover-link' href="${el.link}" target="_blank" rel="noopener noreferrer">
+        </a>
+          <div class='drop-shadow'></div>
+        
+          <li class='ce_container--item'> 
+            <img class='popup_container--hotel-image' src='${el.img}' alt='hotel-img'>
+              <h3 class='ce_container--item__title'>${el.title}</h3>
+            <span class='popup_container--price'>${el.price}</span>
+            
+          </li>
       </ul>
       `;
     document
